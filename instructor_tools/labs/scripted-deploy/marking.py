1fd97462daf221ff2ecff39ef8395d649cdb097a
Elise Guimaraes <eliseguimaraes@gmail.com>
2022-05-29 10:55:36 -0400

    add lo-15 files

---
+++ instructor_tools/labs/scripted-deploy/marking.py
@@ -0,0 +1,17 @@
+import subprocess
+
+subprocess.call("unnistall_wordpress.sh", shell=True)
+
+subprocess.call("Wordpress.sh", shell=True)
+
+def test_httpd_is_running(host):
+    httpd = host.service("httpd")
+    assert httpd.is_running
+
+def test_wp_login_is_file(host):
+    wp_login = host.file("/var/www/html/wp-login.php")
+    assert wp_login.is_file
+
+def test_wp_config_is_file(host):
+    wp_config = host.file("/var/www/html/wp-config.php")
+    assert wp_config.is_file
